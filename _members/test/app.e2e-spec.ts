ac05818cb25d62d5bd52c8d9c0087022ca79f410
Nosachenko-PI19b <59052298+Keronon@users.noreply.github.com>
2023-05-05 22:26:08 +0300

    Tests : prepared e2e
    + profiles - complated

--- _members/test/app.e2e-spec.ts
+++ _members/test/app.e2e-spec.ts
@@ -1,12 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('_members', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
--- _profiles/test/app.e2e-spec.ts
+++ _profiles/test/app.e2e-spec.ts
@@ -2,26 +2,51 @@
 import * as req from 'supertest';
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import { AppModule        } from '../src/app.module';
 import { GetProfileDto    } from '../src/profiles/dto/get-profile.dto';
 import { AccountDto       } from '../src/profiles/dto/account.dto';
 import { RegistrationDto  } from '../src/profiles/dto/registration.dto';
+import { ConfigModule     } from '@nestjs/config';
+import { SequelizeModule  } from '@nestjs/sequelize';
+import { Profile          } from '../src/profiles/profiles.struct';
+import { Comment          } from '../src/comments/comments.struct';
+import { JwtGlobalModule  } from '../src/jwt.module';
+import { ProfilesModule   } from '../src/profiles/profiles.module';
+import { CommentsModule   } from '../src/comments/comments.module';
 
-describe( 'host/api/profiles/', () =>
+describe( '_profiles', () =>
 {
     let app: INestApplication;
-    let admin_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImVtYWlsLTFAbWFpbC5ydSIsImlkIjoxLCJyb2xlIjp7ImlkIjoxLCJuYW1lIjoiQURNSU4iLCJkZXNjcmlwdGlvbiI6ItCw0LTQvNC40L3QuNGB0YLRgNCw0YLQvtGAIiwiY3JlYXRlZEF0IjoiMjAyMy0wNS0wM1QyMDoxMjo1Ni4wODVaIiwidXBkYXRlZEF0IjoiMjAyMy0wNS0wM1QyMDoxMjo1Ni4wODVaIn0sImlhdCI6MTY4MzE0NTAyNSwiZXhwIjoxNjgzMjMxNDI1fQ.wkHhXfdyuKxFU-qmWodBdTHYRVk5tE9MBU6wxOpKbvY';
-    let admin_idUser;
-    let admin_profile;
+    let admin_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImVtYWlsLTFAbWFpbC5ydSIsImlkIjoxLCJyb2xlIjp7ImlkIjoxLCJuYW1lIjoiQURNSU4iLCJkZXNjcmlwdGlvbiI6ItCw0LTQvNC40L3QuNGB0YLRgNCw0YLQvtGAIiwiY3JlYXRlZEF0IjoiMjAyMy0wNC0yMFQxNDozMTozMS43NzVaIiwidXBkYXRlZEF0IjoiMjAyMy0wNC0yMFQxNDozMTozMS43NzVaIn0sImlhdCI6MTY4MzMwNzYxMSwiZXhwIjoxNjgzMzk0MDExfQ.2Xtp9u-fy4LqUjHS2a6ZYGFSJKngxX9-gLOY2hWsmJI';
+    let ids: number[] = [];
 
     beforeAll( async () =>
     {
         const moduleRef: TestingModule = await Test.createTestingModule( {
-            imports: [ AppModule ],
+            imports: [
+                ConfigModule.forRoot({ envFilePath: '.env' }),
+
+                SequelizeModule.forRoot({
+                    dialect: 'postgres',
+                    host: process.env.PG_HOST || 'pg',
+                    port: Number(process.env.PG_PORT) || 5432,
+                    username: process.env.PG_USER || 'postgres',
+                    password: process.env.PG_PASS || 'root',
+                    database: process.env.PG_DB || 'DB_profiles',
+                    models: [Profile, Comment],
+                    autoLoadModels: true,
+                    logging: false
+                }),
+
+                JwtGlobalModule,
+                ProfilesModule,
+                CommentsModule,
+            ],
         } ).compile();
 
         app = moduleRef.createNestApplication();
         await app.init();
+
+        await new Promise(resolve => setTimeout(resolve, 300));
     } );
 
     afterAll( async () => { await app.close(); } );
@@ -37,13 +62,15 @@ describe( 'host/api/profiles/', () =>
         return req( app.getHttpServer() )
             .post( '/api/profiles/reg/user' )
             .send(registrationDto)
-            .expect( 200 )
+            .expect( 201 )
             .expect( (res: req.Response) =>
             {
                 expect( 'idUser' in res.body && typeof(res.body.idUser) === 'number' )
                     .toBe( true );
                 expect( 'token'  in res.body && typeof(res.body.token)  === 'string' )
                     .toBe( true );
+
+                ids.push(res.body.idUser);
             } );
     } );
 
@@ -59,7 +86,7 @@ describe( 'host/api/profiles/', () =>
             .post( '/api/profiles/reg/admin' )
             .set('Authorization', `Bearer ${admin_token}`)
             .send(registrationDto)
-            .expect( 200 )
+            .expect( 201 )
             .expect( (res: req.Response) =>
             {
                 expect( 'idUser' in res.body && typeof(res.body.idUser) === 'number' )
@@ -67,30 +94,28 @@ describe( 'host/api/profiles/', () =>
                 expect( 'token'  in res.body && typeof(res.body.token)  === 'string' )
                     .toBe( true );
 
-                admin_idUser = res.body.idUser;
-                admin_token  = res.body.token;
+                ids.push(res.body.idUser);
+                admin_token = res.body.token;
             } );
     } );
 
     it( 'getProfileByUserId', () =>
     {
         return req( app.getHttpServer() )
-            .get( `/api/profiles/user/${admin_idUser}` )
+            .get( `/api/profiles/user/${ids[1]}` )
             .set('Authorization', `Bearer ${admin_token}`)
             .expect( 200 )
             .expect( (res: req.Response) =>
             {
-                expect( res.body )
-                    .toBeInstanceOf( GetProfileDto );
-
-                admin_profile = res.body;
+                for (const key in new GetProfileDto)
+                    expect(res.body[key]).toBeDefined();
             } );
     } );
 
     it( 'updateAccount', () =>
     {
         const accountDto: AccountDto = {
-            idUser: admin_idUser,
+            idUser: ids[1],
             profileName: 'name',
             email: 'new.mail@mail.ru'
         }
@@ -102,21 +127,34 @@ describe( 'host/api/profiles/', () =>
             .expect( 200 )
             .expect( (res: req.Response) =>
             {
-                expect( res.body )
-                    .toBeInstanceOf( AccountDto );
+                for (const key in new AccountDto)
+                    expect(res.body[key]).toBeDefined();
             } );
     } );
 
-    it( 'deleteAccountByUserId', () =>
+    it( 'deleteAccountByUserId : user', () =>
     {
         return req( app.getHttpServer() )
-            .delete( `/api/profiles/${admin_idUser}` )
+            .delete( `/api/profiles/${ids[0]}` )
             .set('Authorization', `Bearer ${admin_token}`)
             .expect( 200 )
             .expect( (res: req.Response) =>
             {
-                expect( res.body )
-                    .toEqual( 1 );
+                expect( res.text )
+                    .toEqual( '1' );
+            } );
+    } );
+
+    it( 'deleteAccountByUserId : admin', () =>
+    {
+        return req( app.getHttpServer() )
+            .delete( `/api/profiles/${ids[1]}` )
+            .set('Authorization', `Bearer ${admin_token}`)
+            .expect( 200 )
+            .expect( (res: req.Response) =>
+            {
+                expect( res.text )
+                    .toEqual( '1' );
             } );
     } );
 } );
